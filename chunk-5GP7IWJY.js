c1f023163ebdccb42ae37a3739fcd91bc36f8c0c
Facundo Dellanegra <facundodiciembre25@gmail.com>
2024-02-14 17:34:41 -0300

    Auto-generated commit

---
+++ chunk-5GP7IWJY.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunk-WBGOCVB3.js";import"./chunk-AUVHG466.js";import"./chunk-AORHHHYO.js";var n=[{path:"",component:o}];export{n as routes};
---
+++ chunk-R4DHFL6W.js
@@ -0,0 +1,1 @@
+import{e as o}from"./chunk-OE7VE73Q.js";import"./chunk-EBSDOCPU.js";import"./chunk-AORHHHYO.js";var e=[{path:"",component:o}];export{e as routes};
